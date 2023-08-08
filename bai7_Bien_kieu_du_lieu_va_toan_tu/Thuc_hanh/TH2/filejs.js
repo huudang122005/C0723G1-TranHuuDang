e268369552e7abbada0295a673c404978057e1ac
huudang122005 <dang0122005@gmail.com>
2023-08-08 11:07:28 +0700

    nop bai tap 7

---
+++ bai7_Bien_kieu_du_lieu_va_toan_tu/Thuc_hanh/TH2/filejs.js
@@ -0,0 +1,7 @@
+
+inputwidth = +prompt('enter the width');
+inputheight = +prompt('Enter the height');
+let width = parseInt(inputwidth);
+let height = parseInt(inputheight);
+let area = width * height;
+document.write("The area is: " + area);
